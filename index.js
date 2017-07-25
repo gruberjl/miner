fb25be1a9c2c4f5c21c73ad65acf0966154dcdfc
John Gruber <gruberjl@gmail.com>
2017-07-25 14:57:44 -0400

    added linkedin, names, employees, email mining

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const Company = require('./company')
 const linkedin = require('./linkedin')
+const {getEmailAddresses} = require('./crawler')
 const nameParser = require('./name-parser')
 
 const ShawFishmanId = 483315943
 
 const mineCompany = async (company) => {
+  company = await Company.mine.forEmails(company, 3)
+
   if (company.properties.linkedin_company_page && company.properties.linkedin_company_page.value) {
-    const profile = await linkedin.getCompany(company.properties.linkedin_company_page.value)
-    const employees = profile.people
+    company.linked_profile = await linkedin.getCompany(company.properties.linkedin_company_page.value)
+    company.employees = company.linked_profile.people
       .map((person) => Object.assign({}, person, {name: nameParser.parse(person.name)}))
       .filter((employee) => Boolean(employee.name) && employee.name.original !== '')
-
-    console.log(employees)
   }
+
+  console.log(company)
 }
 
 const worker = async (companyId) => {
