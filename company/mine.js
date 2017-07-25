d839ad32396b0a782faedb2ba0e64534b233415e
John Gruber <gruberjl@gmail.com>
2017-07-25 14:46:12 -0400

    trim email addresses

--- company/mine.js
+++ company/mine.js
@@ -3,7 +3,8 @@ const {getEmailAddresses} = require('../crawler')
 const forEmails = async (company, maxDepth=3) => {
   if (company.properties && company.properties.website && company.properties.website.value) {
     const results = await getEmailAddresses(company.properties.website.value, maxDepth)
-    company.properties.mined_emails = { value: results.emailAddresses.join(' ') }
+    const emails = results.emailAddresses.map((email) => email.trim())
+    company.properties.mined_emails = { value: emails.join(' ') }
   }
 
   return company
